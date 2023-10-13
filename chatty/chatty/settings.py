ab8c4aa42fb1f12799985f1168ae2b68b68f0f17
Perez Githumbi <codakniggz145@gmail.com>
2023-10-13 12:44:28 +0300

    installed django-extensions for shell_plus capability

--- chatty/chatty/settings.py
+++ chatty/chatty/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django-extensions",
 ]
 
 ROOT_URLCONF = "chatty.urls"
